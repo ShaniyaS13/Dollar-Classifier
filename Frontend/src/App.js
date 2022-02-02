c2be3566b17a0704a5b4413a972cbac004832c54
ShaniyaS13 <57805735+ShaniyaS13@users.noreply.github.com>
2022-02-01 19:57:49 -0500

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,116 @@
+import { useState, Fragment } from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+/* 
+  Instead of using state within classes, our project uses React Hooks. Which allows us to 
+  use state and other features without writing a class for it, as you will see in the
+  App function. (The first three lines of code in the App function uses React Hooks)
+
+  The App function is our react environment and performs a few important tasks: 
+
+  1. It will create a form on our webpage that will have an input field. The input field is
+  used to upload a file, specifically an image file. There's another input field, that will
+  act like a submit button, which is used to upload the image. 
+
+  2. Once the image is uploaded, we are going to pass along this information to the express
+  backend. Note: This will be further explained below. 
+ */
+
+const App = () => {
+  //These three lines of code below is used to obtain all the information about the file we uploaded
+  const [file, setFile] = useState();
+  const [filename, setFileName] = useState("Choose File");
+  const [prediction, setPrediction] = useState(null);
+  const [error, setError] = useState(null);
+  /*
+    The onChange function is connected to our input field from our form. Once we have selected the
+    image, this function is going to the set the file and the name of the file in the varaibles file and
+    filename. 
+   */
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  /*
+    The onSubmit function is connected to our submit button. Once the file is uploaded, we are 
+    going to pass the file to the express backend (which is located in the backend folder, the server.js
+    file). Before this can be done, we have to create a FormData object and save this in the formData variable
+    as we did below. We can append the file we uploaded to the formData variable, so it can easily passed 
+    to express. As seen in the fetch request below. The fetch request is going to look towards our /upload endpoint, 
+    which is explained in the server.js file.
+
+    Once our image is sent to express, express is going to send that file to our trained model, which
+    is in the model folder, in a file called app.py. This will be further explained in that file.
+
+    Express is then going to return the answer of what the trained model predicted for our image. The answer
+    will either be a fifty dollar bill or a hundred dollar bill. This infromation is saved in the
+    prediction varaible, by using the setPrediction function. 
+
+   */
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+
+    fetch("http://localhost:4000/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPrediction(data);
+      })
+      .catch((err) => setError(err));
+  };
+
+  return (
+    <Fragment>
+      <form onSubmit={onSubmit}>
+        <div className="container mt-4">
+          <h4 className="display-4 text-center mb-4">
+            Common Cents
+            <img
+              src={logo}
+              alt="CommonCents Logo"
+              align="center"
+              width="120"
+              height="90"
+            />
+          </h4>
+        </div>
+
+        <div className="custom-file" align="center">
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={onChange}
+            required
+          />
+          <label className="custom-file-label" htmlFor="customFile">
+            {filename}
+          </label>
+        </div>
+
+        <div className="submitButton" align="center">
+          <input
+            type="submit"
+            value="Upload"
+            className="btn btn-primary btm-block mt-4"
+          />
+        </div>
+      </form>
+      <div className="prediction" align="center">
+        {prediction && <h1>{prediction.prediction}</h1>}
+      </div>
+
+      <div className="error" align="center">
+        {error && <h1>{error}</h1>}
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ model/app.py
@@ -0,0 +1,68 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import json
+import numpy as np
+from flask import Flask, request
+from keras.models import load_model
+from PIL import Image, ImageOps
+
+
+app = Flask(__name__)
+
+
+"""
+    We obtained this file and the keras_model.h5 file from teachable machine.
+
+    We added the result variable at the bottom of the code to hold the result
+    of the prediction. Depending on how you enter in your classes into teachable
+    machine will determine the prediction for your output. 
+
+    For example, in teachable machine, when we uploaded the images the first class 
+    was labeled Fifty and the second class was labeled Hundred. The prediction that 
+    the model returns is an array of arrays, so essentially a 2D array. If the first 
+    element[0][0] is more than 0.5.... that means the image you uploaded is more than
+    likely a Fifty Dollar bill. If the second element[0][1] is more than 0.5... then 
+    the image is a Hundred Dollar bill. Which is why we incorporated the if statment 
+    at the bottom. 
+
+    As previously mentioned, python will return this result if the image is a
+    Fifty Dollar Bill or a Hundred Dollar Bill to server.js and server.js will return 
+    the result to app.js and display it. 
+"""
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Load the model
+    model = load_model('keras_model.h5', compile=False)
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # Replace this with the path to your image
+    image = Image.open(request.files['file'])
+    # resize the image to a 224x224 with the same strategy as in TM2:
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    # print(prediction)
+    result = None
+    #print('gfdgdfg', prediction[0][1])
+    if prediction[0][0] > 0.50:
+        result = 'Fifty'
+    else:
+        result = 'Hundred'
+
+    return {'prediction': result}
